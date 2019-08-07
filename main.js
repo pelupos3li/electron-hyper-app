10f922c55e2ba0a90e994b12337ac6c058e86959
pelupos3li <53795939+pelupos3li@users.noreply.github.com>
2019-08-07 18:36:32 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+ const {
+  app, Menu, Tray, dialog,
+} = require('electron');
+
+const { spawn } = require('child_process');
+const Store = require('electron-store');
+const fixPath = require('fix-path');
+const Sentry = require('@sentry/electron');
+const fs = require('fs');
+
+const Store = require('electron-store');
+const Sentry = require('@sentry/electron');
+
+fixPath();
+
+Sentry.init({ dsn: 'https://18c9943a576d41248b195b5678f2724e@sentry.io/1506479' });
+@ -32,12 +34,10 @@ function getLocale() {
+  switch (locale) {
+    case 'es-419' || 'es':
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/es.json')));
+      break;
+    case 'pt-BR' || 'pt-PT':
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/pt.json')));
+    default: 
+    default:
+      return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/en.json')));
+      break;
+  }
+}
+
+@ -106,6 +106,8 @@ function render(tray = mainTray) {
+  ]);
+
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', tray.popUpContextMenu);
+}
+
+app.on('ready', () => {
